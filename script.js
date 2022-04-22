7f06bd6a21e07cd49301b1fca01b4ef5b0314a8f
Brett <bretttanner171@gmail.com>
2022-04-22 12:25:33 +0900

    Add playArea reset and fix logic errors

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 // get grid size from input form
-// select elements
 const inputForm = document.querySelector("#inputForm");
 const sizeInput = document.querySelector("#sizeInput");
 const playArea = document.querySelector("#playArea");
 
 function makeGrid(event) {
+    // stop form from refreshing the page
     event.preventDefault();
-    let gridSize = sizeInput.value;
-    divBasis = 100 / Math.sqrt(gridSize);
+    // clear previous play area
+    while (playArea.firstChild) {
+        playArea.removeChild(playArea.firstChild);
+    }
+    // get grid size from form
+    const gridWidth = sizeInput.value;
+    const gridSize = gridWidth * gridWidth;
+    // create divs
     for (i = 0; i < gridSize; i++) {
         let div = document.createElement("div");
+        // set basis of divs to have an even number per row
+        divBasis = 100 / gridWidth;
         div.style.flexBasis = `${divBasis}%`;
         playArea.appendChild(div);
     }
